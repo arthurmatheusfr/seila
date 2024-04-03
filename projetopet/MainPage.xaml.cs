a1f2fbf8d46e6c70b0614da6fd8a59df75543aef
arthur <arthurmatheusfreitas@gmail.com>
2024-04-03 08:28:44 -0300

    p/oioi

--- projetopet/MainPage.xaml.cs
+++ projetopet/MainPage.xaml.cs
@@ -2,18 +2,23 @@
 
 public partial class MainPage : ContentPage
 {
-   List<Personagem> PersonagemList = new List<Personagem>();
-	Personagem CurrentPersonagem;
-  int currentPersonagemIndex = 0;
+   
 	public MainPage()
 	{
 		InitializeComponent();
-    
-    PersonagemList.Add(new Capivara());
-    PersonagemList.Add(new Gaivota());
-    PersonagemList.Add(new Jacare());
-	}
-
+  var Personagem1 = new Capivara();
+  var Personagem2 = new Jacare();
+  var Personagem3 = new Gaivota();
+	personagemimagem.Source=atual.GetImageName();
+  }
+  void QuandoClicarnoBotao(Object Sender, EventArgs args)
+  {
+   if (atual==Personagem1)
+      atual = Personagem2;
+    else if(atual==Personagem2)
+    atual = Personagem1;
+    personagemimagem.Source=atual.GetImageName();
+  }
 	
 }
 

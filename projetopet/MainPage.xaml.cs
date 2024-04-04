a3870df203eb0ba0a29b8ac282b695185e2de8fc
arthur <arthurmatheusfreitas@gmail.com>
2024-04-04 11:40:17 -0300

    p/oioi

--- projetopet/MainPage.xaml.cs
+++ projetopet/MainPage.xaml.cs
@@ -2,23 +2,28 @@
 
 public partial class MainPage : ContentPage
 {
-   
-	public MainPage()
-	{
-		InitializeComponent();
-  var Personagem1 = new Capivara();
-  var Personagem2 = new Jacare();
-  var Personagem3 = new Gaivota();
-	personagemimagem.Source=atual.GetImageName();
-  }
-  void QuandoClicarnoBotao(Object Sender, EventArgs args)
-  {
-   if (Atual==Personagem1)
-      Atual = Personagem2;
-    else if(Atual==Personagem2)
-    Atual = Personagem1;
-    personagemimagem.Source=Atual.GetImageName();
-  }
-	
-}
+    private Capivara Personagem1;
+    private Jacare Personagem2;
+    private Gaivota Personagem3;
+    private Personagem Atual;
+    public MainPage()
+    {
+        InitializeComponent();
+        
+        Personagem1 = new Capivara();
+        Personagem2 = new Jacare();
+        Personagem3 = new Gaivota();
+        Atual = Personagem1;
+        personagemimagem.Source = Atual.GetImageName();
+    }
 
+    void QuandoClicarnoBotao(Object sender, EventArgs args)
+    {
+        if (Atual == Personagem1)
+            Atual = Personagem2;
+        else if (Atual == Personagem2)
+            Atual = Personagem1;
+        
+        personagemimagem.Source = Atual.GetImageName();
+    }
+}
